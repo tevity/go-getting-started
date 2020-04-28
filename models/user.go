0b46218ca1e87aa15721e39f35345ecc73a4577d
Kevin Gurton <kevin.gurton@gmail.com>
2020-04-28 15:27:51 +0100

    Added User functions

--- models/user.go
+++ models/user.go
@@ -11,3 +11,16 @@ var (
 	users  []*User
 	nextID = 1
 )
+
+// GetUsers dsaa
+func GetUsers() []*User {
+	return users
+}
+
+//AddUser as
+func AddUser(user User) (User, error) {
+	user.ID = nextID
+	nextID++
+	users = append(users, &user)
+	return user, nil
+}
