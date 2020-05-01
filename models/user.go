c8e8f7db984c6d86111cd0736046f3232ae30cf6
Kevin Gurton <kevin.gurton@gmail.com>
2020-05-01 15:29:47 +0100

    Added new functions with if and for

--- models/user.go
+++ models/user.go
@@ -1,5 +1,10 @@
 package models
 
+import (
+	"errors"
+	"fmt"
+)
+
 //User a user
 type User struct {
 	ID        int
@@ -19,8 +24,47 @@ func GetUsers() []*User {
 
 //AddUser as
 func AddUser(user User) (User, error) {
+	if user.ID != 0 {
+		return User{}, errors.New("User already has an ID")
+	}
+
 	user.ID = nextID
 	nextID++
 	users = append(users, &user)
 	return user, nil
+}
+
+// GetUserByID dfgasg
+func GetUserByID(id int) (User, error) {
+	for _, user := range users {
+		if user.ID == id {
+			return *user, nil
+		}
+	}
+
+	return User{}, fmt.Errorf("User with id '%v' not found", id)
+}
+
+// UpdateUser fdasg
+func UpdateUser(user User) (User, error) {
+	for i, toUpdate := range users {
+		if user.ID == toUpdate.ID {
+			users[i] = &user
+			return user, nil
+		}
+	}
+
+	return User{}, fmt.Errorf("User with id %v not found", user.ID)
+}
+
+// RemoveUserByID dfgaf
+func RemoveUserByID(id int) error {
+	for i, user := range users {
+		if user.ID == id {
+			users = append(users[:i], users[i+1:]...)
+			return nil
+		}
+	}
+
+	return fmt.Errorf("User with id %v not found", id)
 }
